8f1e9f7d91f8d3ea0422b54e65b7ec986ae61101
opsys-saito <k_saito@open-system.co.jp>
2025-05-23 16:39:48 +0900

    breadcrumb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def breadcrumb(*items)
+    tag.nav class: "text-truncate", style: "--bs-breadcrumb-divider: '>';", "aria-label": "breadcrumb" do
+      tag.ol class: "breadcrumb flex-nowrap" do
+        items.each.with_index(1) do |item, i|
+          concat tag.li item, class: ["breadcrumb-item", "text-truncate active": i.eql?(items.size)], "aria-current": i.eql?(items.size) ? :page : nil
+        end
+      end
+    end
+  end
 end
